ad945d97cb7e3707cfadfb960d8b9e4bf787793b
cris <cristina.csevi@gmail.com>
2025-03-03 17:42:30 +0100

    Update of the header

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,11 +1,24 @@
-import Link from "next/link";
-import { SuisseIntlMedium } from "../fonts";
+import Link from "next/link"
+import Image from "next/image"
+import { SuisseIntlMedium } from "../fonts"
 
 export default function Header() {
   return (
-    <header className={`${SuisseIntlMedium.className} fixed top-0 w-full md:w-auto left-0 md:left-auto right-5 pt-6 md:pr-13 z-50`}>
-      <nav>
-        <ul className="flex items-center justify-between md:justify-end md:gap-8 text-foreground px-8 md:px-0 max-w-md mx-auto md:mx-0">
+    <header className={`${SuisseIntlMedium.className} fixed top-0 w-full pt-6 px-8 md:px-13 z-50`}>
+      <nav className="flex justify-between items-center max-w-[1920px] mx-auto">
+        <div className="flex-shrink-0">
+          <Link href="/" className="block p-1 hover:opacity-80 transition-opacity">
+            <Image
+              src="/images/asterisco.svg"
+              alt="Asterisk Logo"
+              width={16}
+              height={16}
+              className="brightness-0 invert"
+              priority
+            />
+          </Link>
+        </div>
+        <ul className="flex items-center gap-8">
           <li>
             <Link href="#about" className="hover:text-[#0055FF]">
               about
